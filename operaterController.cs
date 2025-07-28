b5bef95a1e270c987bbfaf7a73515db3924c07b4
AayushiPansuriya <aayushipansuriya87@gmail.com>
2025-07-28 08:39:47 +0530

    Add files via upload

---
+++ operaterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assi_3_Q2_Aayushi.Controllers
+{
+    public class operaterController : Controller
+    {
+        // GET: operater
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult test2()
+        {
+
+            ViewBag.valueA = 10;
+            ViewBag.valueB = 20;
+           /* ViewBag.Sum = sum;*/
+
+            return View();
+        }
+    }
+}
