cb13ecc0aea0c34370c2c901d2bdd7aa0728b43c
AayushiPansuriya <aayushipansuriya87@gmail.com>
2025-07-28 08:39:08 +0530

    Add files via upload

---
+++ one_AController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assi_3_Q1_Aayushi.Controllers
+{
+    public class one_AController : Controller
+    {
+        // GET: one_A
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test1()
+        {
+            ViewBag.value = 5;
+            return View();
+        }
+    }
+}
